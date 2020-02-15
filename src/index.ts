0f89b890118f60c2b54560207a559fc027abecbf
Ketut Sandiarsa <ktutnik@gmail.com>
2020-02-15 09:09:15 +0800

    Upgrade to rc1.35

--- src/index.ts
+++ src/index.ts
@@ -5,26 +5,29 @@ import Plumier, { bind, response, route, WebApiFacility } from "plumier"
 
 dotenv.config()
 
-export class AuthController {
+export class HomeController {
     @route.get("/")
     index() {
         return response.redirect("/index.html")
     }
+}
 
+export class AuthController {
     @oAuth.redirectUri()
     callback(@bind.oAuthUser() user: oAuth.OAuthUser) {
         return response.postMessage({ status: "Success", user })
     }
 }
 
+const herokuForceHttps = { trustProxyHeader: true, forceHttps: true }
+
 new Plumier()
-    .set(new WebApiFacility({ controller: __filename, trustProxyHeader: true, forceHttps:true }))
+    .set(new WebApiFacility({ controller: __filename, ...herokuForceHttps }))
     .set(new ServeStaticFacility())
     .set(new oAuth.FacebookOAuthFacility())
     .set(new oAuth.GoogleOAuthFacility())
     .set(new oAuth.GitHubOAuthFacility())
     .set(new oAuth.GitLabOAuthFacility())
     .set(new oAuth.TwitterOAuthFacility())
-    .initialize()
-    .then(x => x.listen(process.env.PORT || 8000))
+    .listen(process.env.PORT || 8000)
     .catch(console.error)
